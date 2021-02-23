204a8add454c518d0d5550c49dffa21e25e639df
takasing <takaty2011@gmail.com>
2021-02-23 16:08:00 +0900

    basic generics

---
+++ src/lib/standard/generics.spec.ts
@@ -0,0 +1,58 @@
+import test from 'ava';
+import {
+  create,
+  createInstance,
+  GenericNumber,
+  getProperty,
+  length,
+  Lion,
+  myPathThrough,
+  myPathThrough2,
+  pathThrough,
+} from './generics';
+
+test('path through type test', (t) => {
+  t.is(typeof pathThrough(1), 'number');
+  t.is(typeof pathThrough('path'), 'string');
+  t.is(typeof pathThrough({ key: 'value' }), 'object');
+  t.is(typeof pathThrough(undefined), 'undefined');
+  // null is object!!
+  t.is(typeof pathThrough(null), 'object');
+  // @ts-expect-error
+  t.is(typeof pathThrough<string>(1), 'number');
+});
+
+test('length test', (t) => {
+  t.is(length(['a', 'b']), 2);
+  t.is(length({ a: 'b', length: 3 }), 3);
+});
+
+test('my path through test', (t) => {
+  t.is(myPathThrough('path'), 'path');
+  t.is(myPathThrough2('path'), 'path');
+});
+
+test('generics class test', (t) => {
+  const myGenericNumber = new GenericNumber<number>();
+  myGenericNumber.zeroValue = 0;
+  myGenericNumber.add = (x, y) => {
+    return x + y;
+  };
+  t.is(myGenericNumber.zeroValue, 0);
+  t.is(myGenericNumber.add(1, 3), 4);
+
+  const stringNumeric = new GenericNumber<string>();
+  stringNumeric.zeroValue = '';
+  stringNumeric.add = (x, y) => {
+    return `"${x + y}"`;
+  };
+  t.is(stringNumeric.zeroValue, '');
+  t.is(stringNumeric.add('Hello', 'World'), '"HelloWorld"');
+});
+
+test('get property test', (t) => {
+  const x = { a: 1, b: 2, c: 3 };
+  t.is(getProperty(x, 'a'), 1);
+  // Argument of type '"d"' is not assignable to parameter of type '"a" | "b" | "c"'.
+  // t.is(getProperty(x, 'd'), 1);
+});
---
+++ src/lib/standard/generics.ts
@@ -0,0 +1,66 @@
+interface GenericPathThroughFn {
+  <T>(arg: T): T;
+}
+interface GenericPathThroughFn2<T> {
+  (arg: T): T;
+}
+export const pathThrough = <T>(arg: T): T => {
+  return arg;
+};
+interface Lengthwise {
+  length: number;
+}
+// export const length = <T extends { length: number }>(arg: T): number => {
+export const length = <T extends Lengthwise>(arg: T): number => {
+  return arg.length;
+};
+// export const myPathThrough: <U>(arg: U) => U = pathThrough;
+// can also write as object literal
+// export const myPathThrough: { <U>(arg: U): U } = pathThrough;
+export const myPathThrough: GenericPathThroughFn = pathThrough;
+export const myPathThrough2: GenericPathThroughFn2<string> = pathThrough;
+
+export class GenericNumber<T> {
+  // strictPropertyInitialization=trueだとconstructorで初期化しないと怒られる
+  // しかしclassの場合はconstructor以外で初期化されるケースもある
+  // => assertion modifier "!"
+  // コンパイラにnon-nullを宣言している
+  // 例えばinitialize()メソッドで初期化する場合とか
+  zeroValue!: T;
+  add!: (x: T, y: T) => T;
+}
+
+export const getProperty = <T, K extends keyof T>(obj: T, key: K) => {
+  return obj[key];
+};
+
+// factoryを作るときはconstructorで制約
+export const create = <T>(c: { new (): T }): T => {
+  return new c();
+};
+
+class BeeKeeper {
+  hasMask: boolean;
+  constructor(hasMask: boolean) {
+    this.hasMask = hasMask;
+  }
+}
+class ZooKeeper {
+  nameTag: string;
+  constructor(nameTag: string) {
+    this.nameTag = nameTag;
+  }
+}
+class Animal {
+  numLegs!: number;
+}
+export class Bee extends Animal {
+  keeper!: BeeKeeper;
+}
+export class Lion extends Animal {
+  keeper!: ZooKeeper;
+}
+// TODO: test
+export const createInstance = <A extends Animal>(c: new () => A): A {
+  return new c();
+}
