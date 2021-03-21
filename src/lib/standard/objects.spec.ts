782a49a9a0916ccc64f04932d8618ed1e34761b5
takasing <takaty2011@gmail.com>
2021-03-21 21:42:55 +0900

    tomap

--- src/lib/standard/objects.spec.ts
+++ src/lib/standard/objects.spec.ts
@@ -10,3 +10,17 @@ test('from entries test', (t) => {
     { a: 123, b: 456 }
   );
 });
+
+test('to map test', (t) => {
+  const arr = [
+    { k: 'key1', v: 'value1' },
+    { k: 'key2', v: 'value2' },
+  ];
+  t.deepEqual(
+    arr.reduce((acc, cur) => ({ ...acc, [cur.k]: cur.v }), {}),
+    {
+      key1: 'value1',
+      key2: 'value2',
+    }
+  );
+});
